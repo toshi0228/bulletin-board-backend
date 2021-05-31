5064d907d4d2d43915d4439c0e89a71f9d747381
toshi <etoshi0228@gmail.com>
2021-05-31 10:00:50 +0900

    corsエラー修正

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,4 +1,4 @@
-import { RequestHandler, Request, Response, NextFunction } from "express";
+import { RequestHandler } from "express";
 import { User } from "../entity/User";
 
 // ====================================
